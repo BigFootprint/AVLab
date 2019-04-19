b0bf65437e978612c9d2283924a3a33cf8bd83b7
liquanmin <liquanmin@bytedance.com>
2019-04-19 20:58:14 +0800

    camera2 调用

--- app/src/main/java/com/footprint/avlab/teclab/surfaceview/camera/CameraActivity.java
+++ app/src/main/java/com/footprint/avlab/teclab/surfaceview/camera/CameraActivity.java
@@ -1,14 +1,58 @@
 package com.footprint.avlab.teclab.surfaceview.camera;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.Toast;
 import com.footprint.avlab.R;
 import com.footprint.avlab.base.BJBaseActivity;
+import java.util.Arrays;
 
 /**
  * Created by quanmin.li on 2019/4/19
  */
 public class CameraActivity extends BJBaseActivity {
-    private SurfaceView sv;
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder mSurfaceHolder;
+    private CameraDevice mCameraDevice;
+    private CameraManager mCameraManager; //摄像头管理器
+    private Handler childHandler, mainHandler;
+    private String mCameraID; //摄像头Id 0 为后  1 为前
+    private CameraCaptureSession mCameraCaptureSession;
+
+    /**
+     * 摄像头创建监听
+     */
+    private CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice camera) {//打开摄像头
+            mCameraDevice = camera;
+            //开启预览
+            takePreview();
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {//关闭摄像头
+            if (null != mCameraDevice) {
+                mCameraDevice.close();
+                CameraActivity.this.mCameraDevice = null;
+            }
+        }
+
+        @Override
+        public void onError(CameraDevice camera, int error) {//发生错误
+            Toast.makeText(CameraActivity.this, "摄像头开启失败", Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected int getLayoutId() {
@@ -18,12 +62,97 @@ public class CameraActivity extends BJBaseActivity {
     @Override
     protected void initView() {
         super.initView();
+        mSurfaceView = findViewById(R.id.sv);
 
-        sv = findViewById(R.id.sv);
+        mSurfaceHolder = mSurfaceView.getHolder();
+        mSurfaceHolder.setKeepScreenOn(true);
+        // mSurfaceView添加回调
+        mSurfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) { //SurfaceView创建
+                // 初始化Camera
+                initCamera2();
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) { //SurfaceView销毁
+                // 释放Camera资源
+                if (null != mCameraDevice) {
+                    mCameraDevice.close();
+                    CameraActivity.this.mCameraDevice = null;
+                }
+            }
+        });
     }
 
     @Override
     protected void loadData() {
         super.loadData();
+    }
+
+    @SuppressLint("MissingPermission")
+    private void initCamera2() {
+        HandlerThread handlerThread = new HandlerThread("Camera2");
+        handlerThread.start();
+        childHandler = new Handler(handlerThread.getLooper());
+        mainHandler = new Handler(getMainLooper());
+        mCameraID = String.valueOf(CameraCharacteristics.LENS_FACING_FRONT);//后摄像头
+        //获取摄像头管理
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            //打开摄像头
+            mCameraManager.openCamera(mCameraID, stateCallback, mainHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 开始预览
+     */
+    private void takePreview() {
+        try {
+            // 创建预览需要的 CaptureRequest.Builder
+            final CaptureRequest.Builder previewRequestBuilder =
+                mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            // 将SurfaceView的surface作为CaptureRequest.Builder的目标
+            previewRequestBuilder.addTarget(mSurfaceHolder.getSurface());
+            // 创建CameraCaptureSession，该对象负责管理处理预览请求和拍照请求
+            mCameraDevice.createCaptureSession(Arrays.asList(mSurfaceHolder.getSurface()),
+                new CameraCaptureSession.StateCallback() {
+                    @Override
+                    public void onConfigured(CameraCaptureSession cameraCaptureSession) {
+                        if (null == mCameraDevice) {
+                            return;
+                        }
+                        // 当摄像头已经准备好时，开始显示预览
+                        mCameraCaptureSession = cameraCaptureSession;
+                        try {
+                            // 自动对焦
+                            previewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE,
+                                CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+                            // 打开闪光灯
+                            previewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
+                                CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
+                            // 显示预览
+                            CaptureRequest previewRequest = previewRequestBuilder.build();
+                            mCameraCaptureSession.setRepeatingRequest(previewRequest, null, childHandler);
+                        } catch (CameraAccessException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onConfigureFailed(CameraCaptureSession cameraCaptureSession) {
+                        Toast.makeText(CameraActivity.this, "配置失败", Toast.LENGTH_SHORT).show();
+                    }
+                }, childHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
